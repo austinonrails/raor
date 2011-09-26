adb78b6a1b6819ca9a766bff36a157b118a64717
Ethan Waldo <ewaldo@talho.org>
2011-09-26 01:21:40 -0500

    According to https://github.com/rails/rails/pull/1626 the :scss compressor is a problem that is being addressed.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Raor::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( admin.js adminEvents.js adminUsers.js DateTimePicker.js events.js ext_overrides.js raor.js )
   config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :scss
+  #config.assets.css_compressor = :scss
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
