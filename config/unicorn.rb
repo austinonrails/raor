c968c2e1f235e8f4344b99814cef05f7b52f0ad5
Ethan Waldo <ewaldo@talho.org>
2011-10-24 23:51:08 -0500

    Changing RAILS_ROOT to Rails.root in unicorn config.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -37,7 +37,7 @@ before_fork do |server, worker|
   #
   # Using this method we get 0 downtime deploys.
 
-  old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
+  old_pid = Rails.root + '/tmp/pids/unicorn.pid.oldbin'
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
